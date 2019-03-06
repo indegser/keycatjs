17fa78b7f4478751e5912eccb076d8b6c3dab64c
indegser <indegser@gmail.com>
2019-03-06 19:23:54 +0900

    add worker queue

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let popup;
 
 const baseUri = 'http://localhost:8080';
 
+const promises = [];
+
 const pkb = () => {
   window.addEventListener('message', (e) => {
     if (!e.origin.includes('8080')) return;
@@ -11,37 +13,56 @@ const pkb = () => {
       popup.close();
     }
 
-    const { data: { type, payload } } = e;
+    const { data: { id, type, payload } } = e;
     switch (type) {
       case 'signin':
         localStorage.setItem(PKB_USER_KEY, payload);
         break;
-      case 'tx':
-        alert(`success ${payload}`);
-        break;
-      default:
-        return;
+      default: ;
+    }
+
+    for (const work of promises) {
+      const { res, rej, id: workId } = work;
+      if (workId === id) {
+        res(payload);
+        // TODO. pop work from promises.
+      }
     }
   })
 
-  const openWindow = (url) => {
-    popup = window.open(baseUri + pathname, 'peekaboo', 'height=800,width=640');
-    if (popup) {
-      popup.focus();
-    }
-    return popup;
+  const openWindow = (pathname) => {
+    return new Promise((res, rej) => {
+      const id = performance.now().toString().replace('.', '-');
+      const url = new URL(baseUri + pathname);
+      url.searchParams.append('id', id);
+
+      popup = window.open(url.href, 'peekaboo', 'height=800,width=640');
+  
+      if (popup) {
+        popup.focus();
+      }
+      
+      promises.push({
+        id,
+        res,
+        rej,
+      });
+    });
   }
 
   return {
+    getIdentifier: () => {
+      return localStorage.getItem(PKB_USER_KEY);
+    },
     signin: () => {
-      openWindow('/signin');
+      return openWindow('/signin');
     },
     signout: () => {
 
     },
     transaction: (tx) => {
       const identifier = localStorage.getItem(PKB_USER_KEY);
-      openWindow(`/transaction?identifer=${identifier}&payload=${JSON.stringify(tx)}`);
+      return openWindow(`/transaction?identifier=${identifier}&payload=${JSON.stringify(tx)}`);
     },
   }
 };
