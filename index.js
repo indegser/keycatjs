512c1182a09144224a984554378389947771b591
indegser <indegser@gmail.com>
2019-03-13 15:07:08 +0900

    add ready and config message event

--- index.js
+++ index.js
@@ -5,45 +5,55 @@ let popup;
 const baseUri = 'http://172.16.100.28:8080';
 
 const promises = [];
+const config = {};
 
 const pkb = () => {
   window.addEventListener('message', (e) => {
     if (!e.origin.includes('8080')) return;
+
+    const { data: { id, type, payload } } = e;
+    if (type === 'ready') {
+      popup.postMessage({
+        type: 'config',
+        payload: {
+          ...config,
+          id: performance.now(),
+        },
+      }, baseUri);
+      return;
+    }
+
     if (popup) {
       popup.close();
     }
-
-    const { data: { id, type, payload } } = e;
+    const { res, rej } = promises.filter(w => w.id === id)[0];
+  
     switch (type) {
-      case 'signin':
+      case 'signin': {
         localStorage.setItem(PKB_USER_KEY, payload);
-        break;
-      default: ;
-    }
-
-    for (const work of promises) {
-      const { res, rej, id: workId } = work;
-      if (workId === id) {
         res(payload);
-        // TODO. pop work from promises.
+        break;
       }
+      case 'closed': {
+        rej(type);
+        break;
+      }
+      default: {
+        res(payload);
+      };
     }
   })
 
   const openWindow = (pathname) => {
     return new Promise((res, rej) => {
-      const id = performance.now().toString().replace('.', '-');
       const url = new URL(baseUri + pathname);
-      url.searchParams.append('id', id);
       // TODO. Open 타입을 유저가 정할 수 있게끔 하자. 팝업이나 새 창.
       popup = window.open(url.href, 'peekaboo', 'height=800,width=640');
-  
       if (popup) {
         popup.focus();
       }
       
       promises.push({
-        id,
         res,
         rej,
       });
@@ -60,9 +70,12 @@ const pkb = () => {
     signout: () => {
 
     },
-    // useNetwork: (name, ...nodes) => {
-
-    // },
+    useNetwork: (name, nodes) => {
+      config.network = {
+        name,
+        nodes,
+      };
+    },
     transaction: (tx) => {
       const identifier = localStorage.getItem(PKB_USER_KEY);
       return openWindow(`/transaction?identifier=${identifier}&payload=${JSON.stringify(tx)}`);
